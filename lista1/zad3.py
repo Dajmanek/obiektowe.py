afe45c0563ce141f612bdae77ab839074df73022
Dajmanek <damian.sternik@student.up.krakow.pl>
2021-03-03 23:12:57 +0100

    #

--- lista1/zad3.py
+++ lista1/zad3.py
@@ -38,16 +38,13 @@ time.sleep(0.5)
 points_bot = 0
 points_player = 0
 
-die1 = Die(6)
-die2 = Die(6)
+die = Die(6)
 
 while True:
     print("Rzut komputera...")
-    die1.roll()
-    die2.roll()
-    points_bot += die1.get_value() + die2.get_value()
+    die.roll()
+    points_bot += die.get_value()
     time.sleep(1)
-
     while True:
         print("Twoja kolej (wpisz \"rzut\" aby rzucic kośćmi): ", end='')
         input_text = input().lower()
@@ -56,21 +53,24 @@ while True:
 
     print("Rzucam...")
     time.sleep(1)
-    die1.roll()
-    die2.roll()
-    points_player += die1.get_value() + die2.get_value()
-    print(f"Wylosowales liczby {die1.get_value()} oraz {die2.get_value()}. Posiadasz {points_player} punktów.)")
-
+    die.roll()
+    points_player += die.get_value()
+    print(f"Wylosowales {die.get_value()}pkt. Aktualnie posiadasz {points_player}pkt.")
     while True:
-        print("Wpisz \"koniec\" aby zrezygnować z rzucania i wyświetlić wynik lub \"graj\" aby grać dalej.")
+        print("Wpisz \"koniec\" aby zrezygnować z rzucania i wyświetlić wynik lub \"graj\" aby grać dalej: ", end='')
         input_text = input().lower()
         if input_text == "koniec":
+            if points_player > 21:
+                print("Przegrałeś!")
+                print("Przekroczyłeś 21 punktow.")
+                print(f"Komputer uzyskał {points_bot}pkt.")
+                exit()
             if points_bot == points_player:
-                print(f"Gra zakończyła się remisem ({points_bot} punktów)")
+                print(f"Gra zakończyła się remisem ({points_bot}pkt.])")
                 exit()
             print("Wygrałeś!" if points_player > points_bot else "Przegrałeś :(")
-            print(f"Uzyskałeś {points_player} punktów")
-            print(f"Komputer uzyskał {points_bot} punktów")
+            print(f"Uzyskałeś {points_player}pkt.")
+            print(f"Komputer uzyskał {points_bot}pkt.")
             exit()
         if input_text == "graj":
             break
